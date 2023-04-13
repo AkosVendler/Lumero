f478f55b4a4b8ccdad0cebdb731c94f33edd5ab1
TheShoeBox <73746378+AkosVendler@users.noreply.github.com>
2023-04-13 21:26:55 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ const navLinks = document.querySelector(".nav-links");
 const body = document.querySelector("body");
 const list = document.querySelector(".nav-links a");
 
-menuHamburger.addEventListener('click',() => {
+menuHamburger.addEventListener('click', () => {
     navLinks.classList.toggle('mobile-menu');
-    body.style.overflowY = "hidden";
+    if (navLinks.classList.contains('mobile-menu')) {
+        body.style.overflowY = "hidden";
+    } else {
+        body.style.overflowY = "scroll";
+    }
 });
 
 list.addEventListener('click', () => {
     body.style.overflowY = "scroll";
     navLinks.classList.remove('mobile-menu');
-} )
+});
 
 
 
