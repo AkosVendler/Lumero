568f4e787b38d8c9c2d8a88fc844f5129b6e7ead
TheShoeBox <73746378+AkosVendler@users.noreply.github.com>
2023-04-13 20:54:39 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const menuHamburger = document.querySelector("#btn")
-const navLinks = document.querySelector(".nav-links")
-
+const menuHamburger = document.querySelector("#btn");
+const navLinks = document.querySelector(".nav-links");
+const body = document.querySelector("body");
+const list = document.querySelector(".nav-links a");
 
 menuHamburger.addEventListener('click',() => {
-    navLinks.classList.toggle('mobile-menu')
-})
+    navLinks.classList.toggle('mobile-menu');
+    body.style.overflowY = "hidden";
+});
+
+list.addEventListener('click', () => {
+    body.style.overflowY = "scroll";
+    navLinks.classList.remove('mobile-menu');
+} )
+
+
+
+
 
 
 
